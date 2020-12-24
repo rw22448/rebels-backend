c099e770577530735fccfc97283835743f49dbab
Richard Wang <rw22448@gmail.com>
2020-12-25 01:04:24 +1100

    Setup routes for oc1, add GET summoner by name

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 const express = require('express');
-require('dotenv').config();
+const rateLimit = require('express-rate-limit');
+const axios = require('axios');
+const tftRoute = require('./tft/index');
 
-const PORT = process.env.PORT || 5000;
+require('dotenv').config();
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const PORT = process.env.PORT || 5000;
+const PERSONAL_API_KEY = process.env.PERSONAL_API_KEY;
+
+const oneSecondLimiter = rateLimit({
+  windowMs: 1 * 1000,
+  max: 20,
+});
+
+const twoMinuteLimiter = rateLimit({
+  windowMs: 2 * 1000 * 60,
+  max: 100,
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.use(
+  '/tft/oc1',
+  twoMinuteLimiter,
+  oneSecondLimiter,
+  (req, res, next) => {
+    req.rebelsConfig = {
+      region: 'oc1',
+    };
+
+    next();
+  },
+  tftRoute
+);
+
+app.use((error, req, res, next) => {
+  res.status(error.response.status).json({ message: error.message });
+});
+
+axios.interceptors.request.use((req) => {
+  req.headers['X-Riot-Token'] = PERSONAL_API_KEY;
+  return req;
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ tft/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router({ mergeParams: true });
+
+router.get(
+  '/get-summoner/by-name/:rebelsSummonerName',
+  async (req, res, next) => {
+    if (!req.rebelsConfig.region || !req.params.rebelsSummonerName) {
+      res.status(400).json({ message: 'Bad request' });
+    }
+
+    await axios
+      .get(
+        `https://${req.rebelsConfig.region}.api.riotgames.com/tft/summoner/v1/summoners/by-name/${req.params.rebelsSummonerName}`
+      )
+      .then((result) => {
+        res.status(result.status).json(result.data);
+      })
+      .catch((error) => next(error));
+  }
+);
+
+module.exports = router;
