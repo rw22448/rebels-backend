b207b04a351e4dd7d06938bd4c2215978c526446
Richard Wang <rw22448@gmail.com>
2020-12-27 23:04:51 +1100

    Add static routes for all platform routes

--- tft/index.js
+++ tft/index.js
@@ -14,12 +14,152 @@ const twoMinuteLimiter = rateLimit({
 });
 
 router.use(
+  '/br1',
+  twoMinuteLimiter,
+  oneSecondLimiter,
+  (req, res, next) => {
+    req.rebelsConfig = {
+      region: 'br1',
+    };
+
+    next();
+  },
+  api
+);
+
+router.use(
+  '/eun1',
+  twoMinuteLimiter,
+  oneSecondLimiter,
+  (req, res, next) => {
+    req.rebelsConfig = {
+      region: 'eun1',
+    };
+
+    next();
+  },
+  api
+);
+
+router.use(
+  '/euw1',
+  twoMinuteLimiter,
+  oneSecondLimiter,
+  (req, res, next) => {
+    req.rebelsConfig = {
+      region: 'euw1',
+    };
+
+    next();
+  },
+  api
+);
+
+router.use(
+  '/jp1',
+  twoMinuteLimiter,
+  oneSecondLimiter,
+  (req, res, next) => {
+    req.rebelsConfig = {
+      region: 'jp1',
+    };
+
+    next();
+  },
+  api
+);
+
+router.use(
+  '/kr',
+  twoMinuteLimiter,
+  oneSecondLimiter,
+  (req, res, next) => {
+    req.rebelsConfig = {
+      region: 'kr',
+    };
+
+    next();
+  },
+  api
+);
+
+router.use(
+  '/la1',
+  twoMinuteLimiter,
+  oneSecondLimiter,
+  (req, res, next) => {
+    req.rebelsConfig = {
+      region: 'la1',
+    };
+
+    next();
+  },
+  api
+);
+
+router.use(
+  '/la2',
+  twoMinuteLimiter,
+  oneSecondLimiter,
+  (req, res, next) => {
+    req.rebelsConfig = {
+      region: 'la2',
+    };
+
+    next();
+  },
+  api
+);
+
+router.use(
+  '/na1',
+  twoMinuteLimiter,
+  oneSecondLimiter,
+  (req, res, next) => {
+    req.rebelsConfig = {
+      region: 'na1',
+    };
+
+    next();
+  },
+  api
+);
+
+router.use(
   '/oc1',
   twoMinuteLimiter,
   oneSecondLimiter,
   (req, res, next) => {
     req.rebelsConfig = {
       region: 'oc1',
+    };
+
+    next();
+  },
+  api
+);
+
+router.use(
+  '/tr1',
+  twoMinuteLimiter,
+  oneSecondLimiter,
+  (req, res, next) => {
+    req.rebelsConfig = {
+      region: 'tr1',
+    };
+
+    next();
+  },
+  api
+);
+
+router.use(
+  '/ru',
+  twoMinuteLimiter,
+  oneSecondLimiter,
+  (req, res, next) => {
+    req.rebelsConfig = {
+      region: 'ru',
     };
 
     next();
