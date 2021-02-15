143012c4ce4a2244f4e34605500168d791b615e9
Richard Wang <rw22448@gmail.com>
2021-02-15 15:28:40 +1100

    Change structure of endpoints to use else check

--- tft/api.js
+++ tft/api.js
@@ -5,22 +5,22 @@ const router = express.Router({ mergeParams: true });
 router.get('/get-summoner/by-name/:rebelsSummonerName', async (req, res) => {
   if (!req.rebelsConfig.region || !req.params.rebelsSummonerName) {
     res.status(400).json({ message: 'Bad request' });
-  }
-
-  await axios
-    .get(
-      encodeURI(
-        `https://${req.rebelsConfig.region}.api.riotgames.com/tft/summoner/v1/summoners/by-name/${req.params.rebelsSummonerName}`
+  } else {
+    await axios
+      .get(
+        encodeURI(
+          `https://${req.rebelsConfig.region}.api.riotgames.com/tft/summoner/v1/summoners/by-name/${req.params.rebelsSummonerName}`
+        )
       )
-    )
-    .then((result) => {
-      res.status(result.status).json(result.data);
-    })
-    .catch((error) => {
-      console.log(error);
+      .then((result) => {
+        res.status(result.status).json(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
 
-      res.status(500).json({ message: 'Error fetching data' });
-    });
+        res.status(500).json({ message: 'Error fetching data' });
+      });
+  }
 });
 
 router.get(
@@ -28,40 +28,40 @@ router.get(
   async (req, res) => {
     if (!req.rebelsConfig.region || !req.params.encryptedSummonerId) {
       res.status(400).json({ message: 'Bad request' });
+    } else {
+      await axios
+        .get(
+          `https://${req.rebelsConfig.region}.api.riotgames.com/tft/league/v1/entries/by-summoner/${req.params.encryptedSummonerId}`
+        )
+        .then((result) => {
+          if (result.data.length > 0) {
+            const rankData = result.data[0];
+
+            res.status(result.status).json(rankData);
+          } else {
+            res.status(result.status).json({
+              leagueId: '',
+              summonerId: '',
+              summonerName: '',
+              queueType: '',
+              tier: 'UNRANKED',
+              rank: '',
+              leaguePoints: 0,
+              wins: 0,
+              losses: 0,
+              hotStreak: false,
+              veteran: false,
+              freshBlood: false,
+              inactive: false,
+            });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+
+          res.status(500).json({ message: 'Error fetching data' });
+        });
     }
-
-    await axios
-      .get(
-        `https://${req.rebelsConfig.region}.api.riotgames.com/tft/league/v1/entries/by-summoner/${req.params.encryptedSummonerId}`
-      )
-      .then((result) => {
-        if (result.data.length > 0) {
-          const rankData = result.data[0];
-
-          res.status(result.status).json(rankData);
-        } else {
-          res.status(result.status).json({
-            leagueId: '',
-            summonerId: '',
-            summonerName: '',
-            queueType: '',
-            tier: 'UNRANKED',
-            rank: '',
-            leaguePoints: 0,
-            wins: 0,
-            losses: 0,
-            hotStreak: false,
-            veteran: false,
-            freshBlood: false,
-            inactive: false,
-          });
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-
-        res.status(500).json({ message: 'Error fetching data' });
-      });
   }
 );
 
--- tft/regionalApi.js
+++ tft/regionalApi.js
@@ -6,50 +6,48 @@ const router = express.Router({ mergeParams: true });
 router.get('/get-matches/by-puuid/:puuid', async (req, res) => {
   if (!req.rebelsConfig.region || !req.params.puuid) {
     res.status(400).json({ message: 'Bad request' });
+  } else {
+    const localCount = req.query.count;
+
+    await axios
+      .get(
+        encodeURI(
+          `https://${req.rebelsConfig.region}.api.riotgames.com/tft/match/v1/matches/by-puuid/${req.params.puuid}/ids`
+        ),
+        {
+          params: {
+            count: localCount,
+          },
+        }
+      )
+      .then((result) => {
+        res.status(result.status).json(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
+
+        res.status(500).json({ message: 'Error fetching data' });
+      });
   }
-
-  const localCount = req.query.count;
-
-  await axios
-    .get(
-      encodeURI(
-        `https://${req.rebelsConfig.region}.api.riotgames.com/tft/match/v1/matches/by-puuid/${req.params.puuid}/ids`
-      ),
-      {
-        params: {
-          count: localCount,
-        },
-      }
-    )
-    .then((result) => {
-      res.status(result.status).json(result.data);
-    })
-    .catch((error) => {
-      console.log(error);
-
-      res.status(500).json({ message: 'Error fetching data' });
-    });
 });
 
 router.get('/get-match/by-match-id/:matchId', async (req, res) => {
   if (!req.rebelsConfig.region || !req.params.matchId) {
     res.status(400).json({ message: 'Bad request' });
-  }
-
-  await axios
-    .get(
-      encodeURI(
+  } else {
+    await axios
+      .get(
         `https://${req.rebelsConfig.region}.api.riotgames.com/tft/match/v1/matches/${req.params.matchId}`
       )
-    )
-    .then((result) => {
-      res.status(result.status).json(result.data);
-    })
-    .catch((error) => {
-      console.log(error);
+      .then((result) => {
+        res.status(result.status).json(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
 
-      res.status(500).json({ message: 'Error fetching data' });
-    });
+        res.status(500).json({ message: 'Error fetching data' });
+      });
+  }
 });
 
 module.exports = router;
