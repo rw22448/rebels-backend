c4ce752224daed5b3a443674a8bc59ac47e56a3f
Richard Wang <rw22448@gmail.com>
2021-03-01 21:15:06 +1100

    Unify latest match data call into single endpoint

--- tft/regionalApi.js
+++ tft/regionalApi.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const axios = require('axios');
-const { json } = require('express');
 const router = express.Router({ mergeParams: true });
 
-router.get('/get-matches/by-puuid/:puuid', async (req, res) => {
+router.get('/get-latest-match-details/by-puuid/:puuid', async (req, res) => {
   if (!req.rebelsConfig.region || !req.params.puuid) {
     res.status(400).json({ message: 'Bad request' });
   } else {
     const localCount = req.query.count;
+    let matchIds;
 
     await axios
       .get(
@@ -21,13 +21,80 @@ router.get('/get-matches/by-puuid/:puuid', async (req, res) => {
         }
       )
       .then((result) => {
-        res.status(result.status).json(result.data);
+        matchIds = result.data;
       })
-      .catch((error) => {
-        console.log(error);
-
-        res.status(500).json({ message: 'Error fetching data' });
+      .catch(() => {
+        res
+          .status(500)
+          .json({ message: 'Error fetching data, could not fetch matchIds' });
       });
+
+    if (!matchIds) {
+      res
+        .status(400)
+        .json({ message: 'matchIds query parameter not passed or empty' });
+    } else {
+      try {
+        const array = matchIds;
+
+        if (array.length <= 0) {
+          throw new Error();
+        } else {
+          try {
+            let baseUrl = req.protocol + '://' + req.get('host');
+
+            const results = await Promise.all(
+              array.map(async (matchId) => {
+                return await axios
+                  .get(
+                    `${baseUrl}/tft/${req.rebelsConfig.region}/get-match/by-match-id/${matchId}`
+                  )
+                  .then((result) => {
+                    console.log('Fetched result');
+                    return result.data;
+                  })
+                  .catch(() => {
+                    console.log('Error fetching result');
+
+                    return;
+                  });
+              })
+            );
+
+            console.log('Finished fetching all results');
+
+            var trueLength = 0;
+
+            for (var i = 0; i < results.length; i++) {
+              if (results[i] !== undefined) {
+                trueLength++;
+              }
+            }
+
+            let orderedResults = [];
+            let latestMatchDateTime = null;
+
+            if (trueLength > 0) {
+              orderedResults = results.sort();
+
+              if (orderedResults[0].hasOwnProperty('info')) {
+                latestMatchDateTime = orderedResults[0].info.game_datetime;
+              }
+            }
+
+            res.status(200).json({
+              latestMatchDateTime: latestMatchDateTime,
+              matchIds: array,
+              results: orderedResults,
+            });
+          } catch (error) {
+            res.status(500).json({ message: 'Error fetching data' });
+          }
+        }
+      } catch (error) {
+        res.status(500).json({ message: 'Error fetching data' });
+      }
+    }
   }
 });
 
